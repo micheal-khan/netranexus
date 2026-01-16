03e7fa3185f977be8a461f5daf1e27c1150271d9
Micheal <khanzaidan786@gmail.com>
2026-01-16 18:36:34 +0530

    Project Init

---
+++ src/app/api/layout-data/route.ts
@@ -0,0 +1,71 @@
+import { NextResponse } from "next/server";
+
+const headerData = [
+    { label: 'About us', href: '/#aboutus' },
+    { label: 'Courses', href: '/#courses' },
+    { label: 'Work', href: '/#work' },
+    { label: 'Team', href: '/#team' },
+    { label: 'Contact', href: '/contact' },
+];
+
+const footerData = {
+    brand: {
+        name: "Netra Nexus",
+        tagline: "Empowering businesses with innovative solutions. Let's create something amazing together.",
+        socialLinks: [
+            {
+                icon: "/images/home/footerSocialIcon/twitter.svg",
+                dark_icon: "/images/home/footerSocialIcon/twitter_dark.svg",
+                link: "https://twitter.com"
+            },
+            {
+                icon: "/images/home/footerSocialIcon/linkedin.svg",
+                dark_icon: "/images/home/footerSocialIcon/linkedin_dark.svg",
+                link: "https://linkedin.com/in"
+            },
+            {
+                icon: "/images/home/footerSocialIcon/dribble.svg",
+                dark_icon: "/images/home/footerSocialIcon/dribble_dark.svg",
+                link: "https://dribbble.com"
+            },
+            {
+                icon: "/images/home/footerSocialIcon/instagram.svg",
+                dark_icon: "/images/home/footerSocialIcon/instagram_dark.svg",
+                link: "https://instagram.com"
+            }
+        ]
+    },
+    sitemap: {
+        name: "Sitemap",
+        links: [
+            { name: "Contact us", url: "/contact" },
+            { name: "About us", url: "/#aboutus" },
+            { name: "Work", url: "/#work" },
+            { name: "Services", url: "/#services" },
+            { name: "Pricing", url: "/#pricing" }
+        ]
+    },
+    otherPages: {
+        name: "Other Pages",
+        links: [
+            { name: "Error 404", url: "/not-found" },
+            { name: "Terms & Conditions", url: "/terms-and-conditions" },
+            { name: "Privacy Policy", url: "/privacy-policy" },
+            { name: "Documentation", url: "/documentation" }
+        ]
+    },
+    contactDetails: {
+        name:"Contact Details",
+        address: "81 Rivington Street London EC2A 3AY",
+        email: "hello@awake.agency",
+        phone: "0105 192 3556"
+    },
+    copyright: "©2026 Netra Nexus. All Rights Reserved"
+};
+
+export const GET = async () => {
+  return NextResponse.json({
+    headerData,
+    footerData
+  });
+};
